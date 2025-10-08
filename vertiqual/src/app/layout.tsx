a38f52d3d97511b1046f3b7f855e766bc376f42d
HasnaS-55 <salihihasna02@gmail.com>
2025-10-08 13:17:23 +0100

    cursor

--- vertiqual/src/app/layout.tsx
+++ vertiqual/src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Poppins } from 'next/font/google'
 import localFont from 'next/font/local'
 import './globals.css' 
 import Header from '@/components/Header'
-import BlobCursor from '@/components/BlobCursor';
+
 const poppins = Poppins({ 
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700'],
@@ -30,22 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <body className={`${poppins.variable} ${nohemi.variable} font-poppins`}>
-        <BlobCursor
-  blobType="circle"
-  fillColor="#1C4B24"
-  trailCount={3}
-  sizes={[20, 20, 15]}
-  innerSizes={[10, 18, 14]}
-  innerColor="#D7F78190"
-  opacities={[0.9, 0.7, 0.5]}
-  shadowColor="#1C4B24"
-  shadowBlur={2}
-  useFilter={true}
-  fastDuration={0.1}
-  slowDuration={0.3}
-  filterStdDeviation={8} // Much lower for less blur
-  filterColorMatrixValues="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 18 -5" // Better values
-/>
+        
 
 
         <Header />
